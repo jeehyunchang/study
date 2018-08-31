7ae1be0dd88913c6e0fb1bc07e4be1eb5b6ee89d
jeehyunchang <win8105@nate.com>
2018-08-31 16:34:49 +0900

    ddd
    dd

--- main.go
+++ main.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-  "log"
   "fmt"
-  "net/http"
 )
-func determineListenAddress() (string, error) {
-  port := "5000"
 
-  return ":" + port, nil
-}
-func hello(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "Hello World")
-}
+
 func main() {
-  addr, err := determineListenAddress()
-  if err != nil {
-    log.Fatal(err)
-  }
-  http.HandleFunc("/", hello)
-  log.Printf("Listening on %s...\n", addr)
-  if err := http.ListenAndServe(addr, nil); err != nil {
-    panic(err)
-  }
+  fmt.Println("hello world")
 }
