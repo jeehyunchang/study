e110398bad4587713f6133f52722a782ad823b00
jeehyunchang <win8105@nate.com>
2018-08-31 17:00:54 +0900

    web
    web

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"net/http"
 )
 
-
 func main() {
-  fmt.Println("hello world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":5000", nil)
 }
